de52cb96e10335dbff43b17b7df14097faea028e
shawnjp <shawnjp@gmail.com>
2024-05-13 18:07:53 -0400

    checkin

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import PrideFlag from './components/inclusivePrideFlagColors'; // Import the PrideFlag component
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
         <PrideFlag /> {/* Use the PrideFlag component as a background */}
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
+
