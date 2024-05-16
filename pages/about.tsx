e5159162e578ef424bb078c23134e733873bc17f
shawnjp <34354175+shawnjp@users.noreply.github.com>
2024-05-15 23:22:23 -0400

    Features/users (#2)
    * next
    
    * updates

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Head from "next/head";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className="container">
+      <Head>
+        <title>About Us</title>
+      </Head>
       <h1>About Us</h1>
       <p>This is the about page where you can find information about our company.</p>
+      <style jsx global>{`
+        html, body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
     </div>
   );
 };
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Head from "next/head";
 
 const ContactPage = () => {
   return (
-    <div>
+    <div className="container">
+      <Head>
+        <title>Contact Us</title>
+      </Head>
       <h1>Contact Us</h1>
       <p>Here you can find how to reach us.</p>
+      <style jsx global>{`
+        html, body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
     </div>
   );
 };
